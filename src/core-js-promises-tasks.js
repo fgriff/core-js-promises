cfca46a1d1c6c39cba6d3148761838d2e5e1786b
fgriff <anglirum@gmail.com>
2025-04-13 23:43:40 +0300

    feat: solve queuePromises task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -130,8 +130,14 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce(
+    (acc, promise) =>
+      acc.then((accResult) =>
+        promise.then((promiseResult) => accResult + promiseResult)
+      ),
+    Promise.resolve('')
+  );
 }
 
 module.exports = {
